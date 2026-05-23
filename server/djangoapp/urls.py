1e37fb0a496e48efa6d4b988fc0284d38e61a089
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-23 10:24:26 -0500

    Add user management with React front-end

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,11 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from django.urls import path
+from djangoapp.views import login_user, logout_request, registration
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path('register', registration, name='registration'),
+    path('login', login_user, name='login'),
+    path('logout/', logout_request, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
