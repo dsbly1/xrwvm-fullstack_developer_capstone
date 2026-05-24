6465707873e1000805c14b0325a04da95e20eecb
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-24 18:57:56 -0500

    Refactor exception handling and improve readability

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,21 +10,23 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
-    request_url = backend_url+endpoint+"?"+params
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
+    request_url = backend_url + endpoint + "?" + params
     print("GET from {} ".format(request_url))
     try:
         response = requests.get(request_url)
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
+
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         response = requests.get(request_url)
         return response.json()
@@ -32,11 +34,12 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
