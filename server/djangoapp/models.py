c3cfcd4759fc39f22a58b59911855ef282110d53
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-24 19:11:24 -0500

    Refactor CarModel foreign key definition and string method

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -12,7 +11,8 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(
+        CarMake, null=True, on_delete=models.CASCADE)
     dealer_id = models.IntegerField(null=True)
     name = models.CharField(null=False, max_length=30)
 
