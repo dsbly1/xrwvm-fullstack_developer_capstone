4cafa29aadfcf756f33bd05bfb604a0d9271d6f4
dsbly1 <dsbly1@cougars.ccis.edu>
2026-05-24 19:15:01 -0500

    Fix string quotes in CarModelAdmin list_display

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 2
 
+
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
+
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ["name", "type", "year", "car_make"]
+    list_display = ['name', 'type', 'year', 'car_make']
+
 
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
